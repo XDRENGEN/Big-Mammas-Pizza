b12885a9258801278915cea75742e9c3b2e7f890
Isakgm <Isakgm@gmail.com>
2023-10-30 14:11:38 +0100

    Pizza makers

--- Big Mammas Pizza/model/pizza.cs
+++ Big Mammas Pizza/model/pizza.cs
@@ -1,6 +1,59 @@
 ﻿namespace Big_Mammas_Pizza.model
 {
-    public class pizza
+    public class Pizza
     {
+        //instansfelter
+
+        private string _name;
+        private int  _price;
+        private string _description;
+        private int _pizzaNumber;
+
+        //proberties
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public int Price
+        {
+            get { return _price; }
+            set { _price = value; }
+        }
+
+        public string Description
+        {
+            get { return _description; }
+            set { _description = value; }
+        }
+
+        public int PizzaNumber
+        {
+            get { return _pizzaNumber; }
+            set { _pizzaNumber = value;}
+        }
+        //konstruktør
+
+        public Pizza()
+        {
+            _pizzaNumber = 0;
+            _name = "";
+            _description = "";
+            _price = 0;
+        }
+        public Pizza(int PizzaNummer, string Name, string Description, int Price)
+        {
+            _pizzaNumber= PizzaNummer;
+            _name = Name;
+            _description = Description;
+            _price = Price;
+        }
+
+        public override string ToString()
+        {
+            return $"{{{nameof(Name)}={Name}, {nameof(Price)}={Price.ToString()}, {nameof(Description)}={Description}, {nameof(PizzaNumber)}={PizzaNumber.ToString()}}}";
+        }
     }
 }
---
+++ Big Mammas Pizza/services/Pizzarepository.cs
@@ -0,0 +1,103 @@
+﻿using Big_Mammas_Pizza.model;
+using System.Text;
+
+namespace Big_Mammas_Pizza.services
+{
+    public class PizzaRepository
+    {
+        // instans felt
+        Dictionary<int, Pizza> _katalog;
+
+        // properties
+        public Dictionary<int, Pizza> Katalog
+        {
+            get { return _katalog; }
+            set { _katalog = value; }
+        }
+
+        // konstruktør
+        public PizzaRepository(bool mockData = false)
+        {
+            _katalog = new Dictionary<int, Pizza>();
+
+
+            if (mockData)
+            {
+                PopualtePizzaRepository();
+            }
+        }
+
+        private void PopualtePizzaRepository()
+        {
+            _katalog.Clear();
+
+            _katalog.Add(1, new Pizza(1,"Margherita","Tomato & cheese", 69));
+            //_katalog.Add(2, new Pizza(2,));
+            //_katalog.Add(3, new Pizza(3, ));
+            //_katalog.Add(4, new Pizza(4,));
+            //_katalog.Add(5, new Pizza(5,));
+
+        }
+
+
+
+        /*
+         * metoder
+         */
+        public Pizza Tilføj(Pizza pizza)
+        {
+            _katalog.Add(pizza.PizzaNumber, pizza);
+
+            return pizza;
+        }
+
+        public Pizza Slet(int PizzaNumber)
+        {
+            if (_katalog.ContainsKey(PizzaNumber))
+            {
+                Pizza slettetPizza = _katalog[PizzaNumber];
+                _katalog.Remove(PizzaNumber);
+                return slettetPizza;
+
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public Pizza HentPizza(int Pizzanumber)
+        {
+            if (_katalog.ContainsKey(Pizzanumber))
+            {
+                return _katalog[Pizzanumber];
+
+            }
+            else
+            {
+                // opdaget en fejl
+                throw new KeyNotFoundException("Pizzanumber does not exist");
+
+
+
+                //return null;
+            }
+        }
+
+        public List<Pizza> HentAllePizzaer()
+        {
+            return _katalog.Values.ToList();
+        }
+
+
+       
+
+        public override string ToString()
+        {
+            String pænTekst = String.Join(", ", _katalog.Values);
+
+            return $"{{{nameof(Katalog)}={pænTekst}}}";
+        }
+    
+}
+}
