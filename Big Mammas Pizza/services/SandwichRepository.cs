b820868b1dfc5316aabef37b4daddeabd55e7c95
Juliusonline <juliuspeterk@gmail.com>
2023-10-31 14:23:03 +0100

    SandwichRepository

--- Big Mammas Pizza/services/SandwichRepository.cs
+++ Big Mammas Pizza/services/SandwichRepository.cs
@@ -30,22 +30,17 @@ namespace Big_Mammas_Pizza.services
             {
                 _katalog.Clear();
 
-                _katalog.Add(1, new Sandwich(1, "Margherita", "Tomato & cheese", 69));
-                _katalog.Add(2, new Sandwich(2, "Vesuvio", "Tomato, cheese & ham", 75));
-                _katalog.Add(3, new Sandwich(3, "Capricciosa", "Tomato, cheese, ham and mushrooms", 80));
-                _katalog.Add(4, new Sandwich(4, "Calzone", "Baked pizza with tomato, cheese, ham and mushrooms", 80));
-                _katalog.Add(5, new Sandwich(5, "Quattro Stagioni", "Tomato, cheese, ham, mushrooms, shrimp and peppers", 85));
-                _katalog.Add(6, new Sandwich(6, "Marinara", "Tomato, cheese, shrimps, mussels and garlic", 85));
-                _katalog.Add(7, new Sandwich(7, "Vegetarian", "Tomato, cheese and vegestables", 80));
-                _katalog.Add(8, new Sandwich(8, "Italiana", "tomato, cheese, onion & meetsauce", 75));
-                _katalog.Add(9, new Sandwich(9, "Gorgonzola", "Tomato, gorgonzola, onion and mushrooms", 85));
-                _katalog.Add(10, new Sandwich(10, "Contadina", "Tomato, cheese, mushrooms & olives", 75));
-                _katalog.Add(11, new Sandwich(11, "Naples", "Tomato, cheese, anchovies & olives", 79));
-                _katalog.Add(12, new Sandwich(12, "Vichinga", "Tomato, cheese, ham, mushrooms, peppers, garlic & chili (strong)", 80));
-                _katalog.Add(13, new Sandwich(13, "Calzone Special", "(Not baked) tomato, cheese, shrimp, ham & meat sauce", 80));
-                _katalog.Add(14, new Sandwich(14, "Esotica", "Tomato, cheese, ham, shrimp & pineapple", 80));
-                _katalog.Add(15, new Sandwich(15, "Tonno", "Tomato, cheese, tuna & shrimp", 85));
-                
+                _katalog.Add(1, new Sandwich(65, "Pollo & Pancetta","Kylling, pancetta, rocula, ost & rød pesto", 69));
+                _katalog.Add(2, new Sandwich(66, "Alla Contadina", "Marineret ovnbagt peberfrugt, aubergine, squash, mozzarella & rucola", 69));
+                _katalog.Add(3, new Sandwich(67, "Gorgonzola", "Skinke, gorgonzola, champignon & løg", 69));
+                _katalog.Add(4, new Sandwich(68, "Skinke", "Ost, skinke, champignon & løg", 69));
+                _katalog.Add(5, new Sandwich(69, "Mortadella", "Ost, mortadella, champignon & løg", 69));
+                _katalog.Add(6, new Sandwich(70, "Kødstrimler", "Ost, kødstrimler, champignon paprika", 69));
+                _katalog.Add(7, new Sandwich(71, "Skinke & ost", "Skinke, ost, salat & dressing", 69));
+                _katalog.Add(8, new Sandwich(72, "Tun", "Tun, salat & dressing", 69));
+                _katalog.Add(9, new Sandwich(73, "Parma", "italiensk lufttørret skinke, Grana Padano & rucola", 70));
+                _katalog.Add(10, new Sandwich(74, "Ventricina", "Ventricina (stærk salami), Grana Padano & rucola", 69));
+               
             }
 
 
@@ -64,7 +59,7 @@ namespace Big_Mammas_Pizza.services
             {
                 if (_katalog.ContainsKey(SandwichNumber))
                 {
-                    Pizza slettetSandwich = _katalog[SandwichNumber];
+                    Sandwich slettetSandwich = _katalog[SandwichNumber];
                     _katalog.Remove(SandwichNumber);
                     return slettetSandwich;
 
@@ -93,7 +88,7 @@ namespace Big_Mammas_Pizza.services
                 }
             }
 
-            public List<Pizza> HentAlleSandwiches()
+            public List<Sandwich> HentAlleSandwiches()
             {
                 return _katalog.Values.ToList();
             }
