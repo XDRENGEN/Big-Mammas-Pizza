15f321a0fde9bc48c63c0f97a99cef85d60afbfd
Juliusonline <juliuspeterk@gmail.com>
2023-11-07 12:54:07 +0100

    Noget

--- Big Mammas Pizza/Program.cs
+++ Big Mammas Pizza/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 
 builder.Services.AddSingleton<PizzaRepository>(new PizzaRepository(true));
-
+ 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
